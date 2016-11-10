0e082258283ded51d98d922d0063a9b40da6b211
otto <otto@mgzl.jp>
2016-11-10 16:48:50 +0900

    fix

--- app/DataBaseDefinition.php
+++ app/DataBaseDefinition.php
@@ -2,10 +2,11 @@
 
 const DATA_BASE_DEFINITION = array(
 	'users' => "CREATE TABLE `users` (
-		`id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-		`email` varchar(256) NOT NULL DEFAULT '',
-		`access_token` varchar(255) NOT NULL DEFAULT '',
-		`refresh_token` varchar(255) NOT NULL DEFAULT '',
+		`id` int(21) NOT NULL,
+		`access_token` varchar(255) DEFAULT '',
+		`refresh_token` varchar(255) DEFAULT '',
+		`login_key` varchar(255) DEFAULT NULL,
+		`last_login` int(11) DEFAULT NULL,
 		PRIMARY KEY (`id`)
 	) ENGINE=InnoDB DEFAULT CHARSET=utf8;"
 );
